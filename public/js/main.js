fa0c8ca5fe7cb15173b8d0f361b885490e39dc71
Brent Daugherty <brent@doorty.com>
2011-05-15 20:17:00 -0700

    added socket.io. next step user sign in with socket.io

--- public/js/main.js
+++ public/js/main.js
@@ -7,4 +7,24 @@ $(document).ready(function() {
   var kahnoodles = new Kahnoodles([kahnoodle1, kahnoodle2, kahnoodle3]);
 
   var kahnoodleView = new KahnoodleView({el: $("#kahnoodles"), model: kahnoodle1});
+
+  /**************************************************************************
+   * socket connection with server. 
+   * iOS > 4.2 uses WebSocket, iOS < 4.2 uses long polling
+   **************************************************************************/
+  var socket = new io.Socket('10.0.1.2', { port: 3000 }); 
+  
+  socket.connect();
+  
+  socket.on('connect', function() { 
+    console.log('connected at ' + new Date());
+  }); 
+  
+  socket.on('message', function(json) { 
+    console.log('json: ' + json.sessionId);
+  });
+  
+  socket.on('disconnect', function() { 
+    console.log('disconnected');
+  });
 });
--- server.js
+++ server.js
@@ -12,3 +12,32 @@ require('./routes')(app);
 app.listen(3000);
 
 console.log('Express app started on port 3000');
+
+// socket.io
+var socket = io.listen(app);
+socket.on('connection', function(client) { 
+  
+  console.log('connection!')
+
+  // tell me what is my id
+   client.send( { sessionId: client.sessionId } );
+   
+   console.log('session id = ' + client.sessionId);
+  
+  // tell everyone else my id
+  // client.broadcast(piece);
+  
+  // tell everyone including myself
+  // socket.boradcast(piece);
+  
+  client.on('message', function(json) { 
+
+    client.broadcast(json);
+    
+  });
+  
+  client.on('disconnect', function() { 
+    
+  });
+  
+});
